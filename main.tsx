f84a3d676eb718f85eb8405fbaa4502a874a25d8
罗楚健 <12858871+cnluocj@user.noreply.gitee.com>
2025-03-14 12:29:17 +0800

    fix

--- main.tsx
+++ main.tsx
@@ -1,5 +1,4 @@
 import blog, { ga, redirects } from "https://deno.land/x/blog/blog.tsx";
-import { unocss_opts } from "./unocss.ts";
 
 blog({
   author: "Dino",
@@ -13,17 +12,4 @@ blog({
     { title: "Twitter", url: "https://twitter.com/denobot" },
   ],
   lang: "en",
-  // localised format based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
-  dateFormat: (date) =>
-    new Intl.DateTimeFormat("en-GB", { dateStyle: "long" }).format(date),
-  middlewares: [
-    ga("UA-XXXXXXXX-X"),
-    redirects({
-      "/foo": "/my_post",
-      // you can skip leading slashes too
-      "bar": "my_post2",
-    }),
-  ],
-  unocss: unocss_opts, // check https://github.com/unocss/unocss
-  favicon: "favicon.ico",
 });
