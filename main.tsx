3182f5b690cbecc5ff60577bf6baeaab9c6eec07
罗楚健 <12858871+cnluocj@user.noreply.gitee.com>
2025-03-14 12:26:28 +0800

    fix

--- main.tsx
+++ main.tsx
@@ -1,14 +1,29 @@
-import blog from "https://deno.land/x/blog/blog.tsx";
+import blog, { ga, redirects } from "https://deno.land/x/blog/blog.tsx";
+import { unocss_opts } from "./unocss.ts";
 
 blog({
-  author: "luocj",
-  title: "Blog",
+  author: "Dino",
+  title: "My Blog",
   description: "The blog description.",
-  avatar: "https://deno-avatar.deno.dev/avatar/83a531.svg",
+  avatar: "avatar.png",
   avatarClass: "rounded-full",
   links: [
-    { title: "Email", url: "cnluocj@gmail.com" },
-    { title: "GitHub", url: "https://github.com/cnluocj" },
+    { title: "Email", url: "mailto:bot@deno.com" },
+    { title: "GitHub", url: "https://github.com/denobot" },
+    { title: "Twitter", url: "https://twitter.com/denobot" },
   ],
-  lang: "zh",
+  lang: "en",
+  // localised format based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
+  dateFormat: (date) =>
+    new Intl.DateTimeFormat("en-GB", { dateStyle: "long" }).format(date),
+  middlewares: [
+    ga("UA-XXXXXXXX-X"),
+    redirects({
+      "/foo": "/my_post",
+      // you can skip leading slashes too
+      "bar": "my_post2",
+    }),
+  ],
+  unocss: unocss_opts, // check https://github.com/unocss/unocss
+  favicon: "favicon.ico",
 });
